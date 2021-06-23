cb638b20c9a320c062434ae1efbd4b82d874d532
Kyle Hekkers <kyle.hekkers@gmail.com>
2021-06-23 13:58:06 -0500

    Update Repo

--- CodeTheWay/CodeTheWay.Web.Ui/Repositories/BarrelsRepository.cs
+++ CodeTheWay/CodeTheWay.Web.Ui/Repositories/BarrelsRepository.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using CodeTheWay.Web.Ui.Models;
+
 namespace CodeTheWay.Web.Ui.Repositories
 {
-    public class IBarrelsRepository
+    public class BarrelsRepository
     {
+        private AppDbContext AppDbContext;
+        public async Task<Barrels> Update(Barrels model)
+        {
+            var result = AppDbContext.Barrels.Update(model);
+            await AppDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
     }
 }
--- CodeTheWay/CodeTheWay.Web.Ui/Repositories/IBarrelsRepository.cs
+++ CodeTheWay/CodeTheWay.Web.Ui/Repositories/IBarrelsRepository.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using CodeTheWay.Web.Ui.Models;
+
 namespace CodeTheWay.Web.Ui.Repositories
 {
     interface IBarrelsRepository
     {
+        public Task<Barrels> Update(Barrels model);
     }
 }
