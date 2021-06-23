532131204ee7005ffeb9557ff87d9646636a95b3
Emily <eaberger2@wisc.edu>
2021-06-23 14:23:26 -0500

    Service Complete

--- CodeTheWay/CodeTheWay.Web.Ui/Services/BarrelsService.cs
+++ CodeTheWay/CodeTheWay.Web.Ui/Services/BarrelsService.cs
@@ -15,5 +15,29 @@ namespace CodeTheWay.Web.Ui.Services
         {
             this.BarrelsRepo = new BarrelsRepository(dbContext);
         }
+
+        public async Task<List<Barrels>> GetBarrels()
+        {
+            return await this.BarrelsRepo.GetBarrels();
+
+        }
+        public async Task<Barrels> GetBarrel(Guid id)
+        {
+            return await this.BarrelsRepo.GetBarrel(id);
+        }
+        public async Task<Barrels> Create(Barrels barrel)
+        {
+            return await this.BarrelsRepo.Create(barrel);
+
+        }
+        public async Task<Barrels> Delete(Barrels model)
+        {
+            return await BarrelsRepo.Delete(model);
+        }
+        public async Task<Barrels> Update(Barrels model)
+        {
+            return await BarrelsRepo.Update(model);
+
+        }
     }
 }
--- CodeTheWay/CodeTheWay.Web.Ui/Services/IBarrelsService.cs
+++ CodeTheWay/CodeTheWay.Web.Ui/Services/IBarrelsService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodeTheWay.Web.Ui.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,10 @@ namespace CodeTheWay.Web.Ui.Services
 {
     interface IBarrelsService
     {
+        public Task<List<Barrels>> GetBarrels();
+        public Task<Barrels> GetBarrel(Guid id);
+        public Task<Barrels> Create(Barrels barrel);
+        public Task<Barrels> Delete(Barrels model);
+        public Task<Barrels> Update(Barrels model);
     }
 }
